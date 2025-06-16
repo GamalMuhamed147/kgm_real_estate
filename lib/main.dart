ee0b8ca86c5da74450beb372886d52260ab63141
Gamal Muhamed <gamalmuhamed74@gmail.com>
2025-06-16 20:56:51 +0400

    Initial commit - Flutter real estate project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RealEstateApp());
+}
+
+class RealEstateApp extends StatelessWidget {
+  const RealEstateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KGM Real Estate',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.teal,
+        fontFamily: 'Roboto',
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('KGM Real Estate')),
+      ),
+    );
+  }
+}
